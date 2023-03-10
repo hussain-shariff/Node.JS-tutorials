6e67ca591e4ea73fb6634f369dba363b58605c3f
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-10 15:30:45 +0530

    express basics

---
+++ 2-express-tutorial/final/http-basics.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.status(200).send('Home Page')
+})
+app.get('/about', (req, res)=>{
+    res.status(200).send('About Page')
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('<h1>Page Not Found </h1>')
+})
+
+app.listen(5000)
