4ace749a0b4272e27ac5800ddebf33e29f84ef16
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-10 12:52:03 +0530

    stream-HTTP-module

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const https = require('http')
+const fs = require('fs')
+
+const server = https.createServer((req, res)=>{
+    const fileStream = fs.createReadStream('./content/bigFile.txt', 'utf8');
+    fileStream.on('open', ()=>{
+        fileStream.pipe(res)
+    })
+    fileStream.on('error', (err)=>{
+        res.end(err);
+    })
+})
+
+server.listen(3000)
