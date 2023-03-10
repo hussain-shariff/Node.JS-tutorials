62aa077d0f7d2a784f814e9c6b8338e1045409b7
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-10 15:14:57 +0530

    http-app

--- 2-express-tutorial/http-header.js
+++ 2-express-tutorial/http-header.js
@@ -1,11 +1,28 @@
 const http = require('http')
+const {readFileSync} = require('fs')
+
+const homePageHtml = readFileSync('./NavBar/index.html')
+const homePageCss = readFileSync('./NavBar/style.css')
+const homePageJs = readFileSync('./NavBar/browser-app.js')
 
 const server = http.createServer((req, res)=>{
     const url = req.url
     // Home Page
     if(url === "/"){
         res.writeHead(200, {'content-type' : 'text/html'})
-        res.write('<h1>Home Page</h1>')
+        res.write(homePageHtml)
+        res.end()
+    }
+    // Home Page CSS
+    else if(url === "/style.css"){
+        res.writeHead(200, {'content-type' : 'text/css'})
+        res.write(homePageCss)
+        res.end()
+    }
+    // Home Page JS
+    else if(url === "/browser-app.js"){
+        res.writeHead(200, {'content-type' : 'text/javascript'})
+        res.write(homePageJs)
         res.end()
     }
     // About Page
