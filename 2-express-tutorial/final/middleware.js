2738163dd96c594710cc1f89c4a6372513fa37c8
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-11 20:54:11 +0530

    third party middelware

--- 2-express-tutorial/final/middleware.js
+++ 2-express-tutorial/final/middleware.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const logger = require('../middlewares/logger')
 const authorize = require('../middlewares/authorize')
+const morgon = require('morgan')
 const app = express()
 
 // setup middleware
-app.use([authorize, logger])
+app.use([authorize, logger, morgon('tiny')])
 
 app.get('/', (req, res)=>{
     res.send("Welcome to home page")
+    console.log(req.user);
 })
 app.get('/about', (req, res)=>{
     res.send("This is about page")
