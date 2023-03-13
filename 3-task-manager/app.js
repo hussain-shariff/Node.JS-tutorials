8663ada68c34bd74773ed3de4c84dda02abb1e9b
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-13 12:51:05 +0530

    routes and controllers setup

--- 3-task-manager/app.js
+++ 3-task-manager/app.js
@@ -1,1 +1,11 @@
-console.log('Task manager');
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+app.use('/api/v1/tasks', tasks)
+
+app.get('/', (req, res)=>{
+    res.send('Hello world')
+})
+
+app.listen(5000)
---
+++ 3-task-manager/controllers/tasks.js
@@ -0,0 +1,28 @@
+
+const getTasks = (req, res)=>{
+    res.send('all items')
+}
+
+const createTask = (req, res)=>{
+
+}
+
+const getSingleTask = (req, res)=>{
+
+}
+
+const updateTask = (req, res)=>{
+
+}
+
+const deleteTask = (req, res)=>{
+
+}
+
+module.exports = {
+    getTasks,
+    createTask,
+    getSingleTask,
+    updateTask,
+    deleteTask
+}
