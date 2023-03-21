5fd110ff16f891ba997094224003ba9d649385d2
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-21 15:58:51 +0530

    final commit

--- 6-jobs-api-project/server/controllers/auth.js
+++ 6-jobs-api-project/server/controllers/auth.js
@@ -14,13 +14,17 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const { email, password } = req.body
     const user = await authModel.findOne({email})
-    const checkPassword = await user.comparePasswords(password)
-    if(!user || !checkPassword){
+    if(!user){
         res.status(404).send('Invalid Credentials.')
     }
     else{
-        const token = user.createJWT()
+        const checkPassword = await user.comparePasswords(password)
+        if(!checkPassword){
+            res.status(404).send('Invalid Credentials.')
+        }else{
+            const token = user.createJWT()
         res.json({user : user.username, token})
+        }
     }
 }
 
