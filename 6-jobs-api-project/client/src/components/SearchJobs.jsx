d3ef3aa71eae0d92223e45deb29e80d9cf416b18
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-27 16:57:52 +0530

    search options

---
+++ 6-jobs-api-project/client/src/components/SearchJobs.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import JobInput from './JobInput'
+import Select from 'react-select'
+import {
+    statusOptions,
+    sortOptions,
+    jobTypeOptions
+} from '../searchOptions'
+
+function SearchJobs() {
+    const [searchDetails, setSearchDetails] = useState({
+        Search : '',
+        status : '',
+        type : '',
+        sort : ''
+    })
+
+    const handleSearch = (e) =>{
+        setSearchDetails(prev=>({
+            ...searchDetails,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+  return (
+    <div className='text-black p-10 bg-white/20 rounded-md mb-5'>
+        <h1 className=' text-white text-center text-2xl md:text-3xl'>Search Form</h1>
+        <div className='grid grid-cols-1 gap-x-5 gap-y-2 md:grid-cols-3 md:gap-y-3 mt-5'>
+            <JobInput
+                name='Search'
+                handleChange={handleSearch}
+                value={ searchDetails.name }
+                type="text"/>
+            <Select 
+                placeholder={'status'}
+                options={statusOptions}
+                isClearable={true}
+                isSearchable={false} />
+            <Select
+                placeholder={"type"}
+                options={jobTypeOptions}
+                isClearable={true}
+                isSearchable={false} />
+            <Select 
+                placeholder={"sort"}
+                options={sortOptions}
+                isClearable={true} 
+                isSearchable={false}/>
+            <button className='bg-white/30 rounded-md w-36 hover:bg-white/40
+            transition ease-out duration-300 py-2 md:py-0 text-white'>
+                Clear filters
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default SearchJobs
