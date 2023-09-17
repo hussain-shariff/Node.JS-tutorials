09b227bc62dac27a3a8ca5be80d25f5ab192416c
hussain-shariff <hussainowaiz070@gmail.com>
2023-09-17 21:12:17 +0530

    test ```
    ```Bash
    $ git add --all
    ```
    
    ```Bash
    $ git commit -m "test"
    [main 0a0d4e4] test
     1 file changed, 1 insertion(+), 1 deletion(-)
    ```
    
    ```Bash
    $ git push
    Enumerating objects: 5, done.
    Counting objects: 100% (5/5), done.
    Delta compression using up to 4 threads
    Compressing objects: 100% (3/3), done.
    Writing objects: 100% (3/3), 309 bytes | 309.00 KiB/s, done.
    Total 3 (delta 2), reused 0 (delta 0), pack-reused 0
    remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
    To

--- 6-jobs-api-project/server/controllers/auth.js
+++ 6-jobs-api-project/server/controllers/auth.js
@@ -1,27 +1,28 @@
-import authModel from "../model/authSchema.js"
+import authModel from '../model/authSchema.js'
 
 export const register = async (req, res) => {
-	try {
-		const user = await authModel.create({ ...req.body })
-		const token = user.createJWT()
-		res.json({ user: user.username, token })
-	} catch (error) {
-		res.status(404).send("Email already exists.")
-	}
+    try {
+        const user = await authModel.create({...req.body })
+        const token = user.createJWT()
+        res.json({ user : user.username , token})
+    } catch (error) {
+        res.status(404).send('Email already exists.')
+    }
 }
 
 export const login = async (req, res) => {
-	const { email, password } = req.body
-	const user = await authModel.findOne({ email })
-	if (!user) {
-		res.status(404).send("Invalid Credentials.")
-	} else {
-		const checkPassword = await user.comparePasswords(password)
-		if (!checkPassword) {
-			res.status(404).send("Invalid Credentials.")
-		} else {
-			const token = user.createJWT()
-			res.json({ user: user.username, token })
-		}
-	}
+    const { email, password } = req.body
+    const user = await authModel.findOne({email})
+    if(!user){
+        res.status(404).send('Invalid Credentials.')
+    }
+    else{
+        const checkPassword = await user.comparePasswords(password)
+        if(!checkPassword){
+            res.status(404).send('Invalid Credentials.')
+        }else{
+            const token = user.createJWT()
+        res.json({user : user.username, token})
+        }
+    }
 }
